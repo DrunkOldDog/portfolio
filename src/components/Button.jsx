2a415724bf307276ba35946044699c51e0b7c108
Juani Reyes <juanisoul@gmail.com>
2022-03-24 23:10:02 -0400

    Add default page structure

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  color: #1d1d1d;
+  background-color: ${(props) =>
+    props.variant === "outlined" ? "transparent" : "#fff"};
+  padding: 15px 18px;
+  border: 0;
+  border-radius: 5px;
+  font-weight: 700;
+  cursor: pointer;
+  border: 2px solid transparent;
+  transition: all .25s;
+
+  &:hover {
+    background-color: transparent;
+    color: #fff;
+    border: 2px solid #fff;
+  }
+`;
