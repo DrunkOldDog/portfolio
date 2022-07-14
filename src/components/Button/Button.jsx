d673b6bffb10a9b9c2cb31d1731443a3cbaa06a5
Juan Reyes <juan@dreamcode.io>
2022-07-13 22:48:00 -0400

    Install chakra and replace styled-components with emotion

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "@emotion/styled";
 
 export const Button = styled.button`
   color: #1d1d1d;
--- src/components/SocialNetworks/SocialNetworks.jsx
+++ src/components/SocialNetworks/SocialNetworks.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "@emotion/styled";
 import { Spotify, Instagram, LinkedIn, GitHub } from "../../common/icons";
 import { useAnalyticsEvent } from "../../common/hooks";
 
