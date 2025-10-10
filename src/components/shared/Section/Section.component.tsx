3e58c150bc7301eb04d63af3289facf9936fc5d3
Juan Reyes <juanisoul@gmail.com>
2025-10-10 16:08:46 -0400

    Minimize images size

--- src/components/shared/Section/Section.component.tsx
+++ src/components/shared/Section/Section.component.tsx
@@ -11,7 +11,7 @@ export default function Section({
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
-      transition={{ duration: 0.5, delay: 0.5 }}
+      transition={{ duration: 0.5, delay: 0.3 }}
       {...props}
     >
       {children}
